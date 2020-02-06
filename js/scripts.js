8879685cf32c1e0ef6638b781220a55d3fd0815e
Priyaraj7 <suppichala@gmail.com>
2020-02-05 18:51:55 -0800

    update background color

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,41 @@
-$(document).ready(function () {
-    $("form#beepboop").submit(function () {
-        event.preventDefault();
+$(document).ready(function() {
+  $("form#beepboop").submit(function() {
+    event.preventDefault();
 
-        var input = parseInt($("input#number").val());
-        var array = [];
-        
+    var input = parseInt($("input#number").val());
+    if (input === "") {
+      alert("Please enter a number");
+      return;
+    }
+    if (Number.isNaN(input)) {
+      alert("Please enter a number");
+      return;
+    }
+    if (input <= 0) {
+      alert("Please enter a number > 1");
+      return;
+    }
+    var array = [];
 
-        for (i = 1; i <= input; i++) {
-            var start = i.toString();
+    for (i = 1; i <= input; i++) {
+      var start = i.toString();
 
-            if (start.includes("1")) {
-                array.push("Beep");
-            } else if (start.includes("2")) {
-                array.push("Boop");
-            } else if (start.includes("3")) {
-                array.push("'I'm sorry, Dave. I'm afraid I can't do that.'");
-            }
-            else {
-                array.push(" " + i);
-            }
-        }
-        var result = $("#result ul").empty();
+      if (start.includes("1")) {
+        array.push("Beep");
+      } else if (start.includes("2")) {
+        array.push("Boop");
+      } else if (start.includes("3")) {
+        array.push("'I'm sorry, Dave. I'm afraid I can't do that.'");
+      } else {
+        array.push(" " + i);
+      }
+    }
+    var result = $("#result ul").empty();
 
-        array.forEach(function(val) {
-            $('<li>').text(val).appendTo(result);
-        });
-        
-     
+    array.forEach(function(val) {
+      $("<li>")
+        .text(val)
+        .appendTo(result);
     });
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
